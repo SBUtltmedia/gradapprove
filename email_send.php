fd361d7ae501e37411682b59a80250699786d54b
DDEV User <nobody@example.com>
2025-03-07 18:49:42 -0500

    testing server name

--- email_send.php
+++ email_send.php
@@ -1,9 +1,11 @@
 <?php
 function sendEmail($rowId, $approvalId, $to) {
+    $server = $_SERVER['SERVER_NAME'];
+
     $to = "$to";
     $subject = "New Student Enrollment Alert!"; ////////// "First Name" "Last name" has requested a thessis approval fo r"column H"
 
-    $message = "You can access the new records here and give your approval: https://gradapprove.ddev.site/show_row.html?rowId=$rowId&approvalId=$approvalId";
+    $message = "You can access the new records here and give your approval: https://$server/show_row.html?rowId=$rowId&approvalId=$approvalId";
 
     $headers = "From: tltmedialab@connect.stonybrook.edu" . "\r\n" .
                "Reply-To: tltmedialab@connect.stonybrook.edu" . "\r\n" .
