00b00b6df2f9707149d746a212d11fba95491a79
Priya Prakash <57007251+PriyaPrakashReveuse@users.noreply.github.com>
2025-03-07 19:06:07 -0500

    tetsing server name 2

--- email_send.php
+++ email_send.php
@@ -12,7 +12,7 @@ function sendEmail($rowId, $approvalId, $to) {
     $to = "$to";
     $subject = "New Student Enrollment Alert!"; ////////// "First Name" "Last name" has requested a thessis approval fo r"column H"
 
-    $message = "You can access the new records here and give your approval: https://$server/show_row.html?rowId=$rowId&approvalId=$approvalId";
+    $message = "You can access the new records here and give your approval: https://$server/gradapprove/show_row.html?rowId=$rowId&approvalId=$approvalId";
 
     $headers = "From: tltmedialab@connect.stonybrook.edu" . "\r\n" .
                "Reply-To: tltmedialab@connect.stonybrook.edu" . "\r\n" .
