7dd8800c4912ea692ef6f6d7a14ca104c24ec5a0
Priya Prakash <57007251+PriyaPrakashReveuse@users.noreply.github.com>
2025-04-09 13:27:57 -0400

    Fixed the code to read the data from the sheet and update yes or no

--- get_cell.php
+++ get_cell.php
@@ -2,14 +2,21 @@
 require 'vendor/autoload.php';
 require 'spreadsheet.php';
 
-$spreadsheetId = $_GET["sheetId"];
+$sheetId = $_GET["sheetId"];
 $rowId = $_GET["rowId"];
 $approvalId = $_GET["approvalId"];
 
-$spreadsheet = new Spreadsheet($spreadsheetId);
+// $sheetId = "1A6RsAaSj6YEbwER22qyJXuQHWU43USiNj-qSSrgKdcU";
+// $rowId = 2;
+// $approvalId = 1;
+
+
+$spreadsheet = new Spreadsheet($sheetId);
 $columnName = "Approval $approvalId";
+// print($columnName);
 
 $dataJson = json_decode($spreadsheet->getRange("A$rowId:Z$rowId", true), true); 
+// print_r($dataJson);
 
 $approval_status = $dataJson[0][$columnName] ?? ""; 
 echo json_encode(["approval_status" => $approval_status]);
